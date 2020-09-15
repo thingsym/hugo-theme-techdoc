e7666cd042c8ab7e4a9011a7729c75c0305348e5
thingsym <thingsym@gmail.com>
2020-09-15 20:21:35 +0900

    chore: fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 module.exports = {
   mode: "production",
-	entry: './src/js/main.js',
-	output: {
+  entry: './src/js/main.js',
+  output: {
     filename: '../static/js/bundle.js',
   },
-	module: {
-		rules: [
-			{
-				test: /.jsx?$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ],
+  },
 };
